7bbfc7c06a37f07103b768b510b64bbb577fc4dc
krisun09 <christina.j.s@outlook.com>
2023-05-28 23:19:15 +1000

    Added comments and javadoc.

--- src/main/java/au/edu/sydney/brawndo/erp/spfea/contact/InvoiceSender.java
+++ src/main/java/au/edu/sydney/brawndo/erp/spfea/contact/InvoiceSender.java
@@ -7,6 +7,12 @@ import au.edu.sydney.brawndo.erp.ordering.Customer;
  * Represents the strategy for sending invoices
  */
 public interface InvoiceSender {
+    /**
+     * Sends a message to a customer
+     * @param token token
+     * @param customer the destination customer
+     * @param data the content of the message
+     */
     void sendInvoice(AuthToken token, Customer customer, String data);
 }
 
